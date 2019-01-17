92ec95bed075ce574b8e7dbdf2de8f14290c54de
Justinas <justinas@eofnet.lt>
2019-01-17 15:10:37 +0200

    few more additions

--- Install.sh
+++ Install.sh
@@ -1,6 +1,77 @@
 #!/bin/sh
-echo "Time for some updates..."
-brew update
-brew install bash gnuls
+# (c) sometime justinas@eofnet.lt
+# If you want to install additional software please add it to packages.txt file, one per line
+# 
+
+detectXcodeTools() {
+echo "Detecting if xcode tools are installed..."
+if ! [ -x "$(command -v git)" ]; then
+echo "Xcode tools does not installed, installing them..."
+xcode-select --install
+else
+echo "Xcode tools already installed on the system!"
+fi 
+}
+
+detectBrew() {
+echo "Detecting if brew package manager is installed..."
+if ! [ -x "$(command -v brew)" ]; then
+echo "Brew does not installed, installing..."
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Brew installed!"
+else
+echo "Brew is already installed!"
+fi
+}
+
+upgradeBash() {
+echo "Detecting if bash is installed/upgraded from ports..."
+if ! [ -x "$(command -v /usr/local/bin/bash)" ]; then
+echo "Bash upgrade is needed..."
+brew install bash
 echo "Please enter your password for permission to change the bash to the newer version!"
 chsh -s /usr/local/bin/bash
+else
+echo "Bash is already up to date!"
+fi
+}
+
+installSoft() {
+if [ -e "packages.txt" ]; then
+echo "You are about to install software that are specified in packages.txt file, are you ready ? (y,n)"
+read answer
+if [ "$answer" == "y" ]; then
+while read -r package; do
+    if ! [ -x "$(command -v $package)" ]; then
+    echo "Package $package does not installed in your system!"
+    brew install $package
+    fi
+done < packages.txt
+else
+echo "Software installation was cancelled!"
+fi
+fi
+}
+
+updateProfile() {
+if [ -e "$HOME/.bash_profile" ]; then
+echo "We are about to override .bash_profile settings, but it seems that you have it already, continue to overwrite file ? (y,n)"
+read answer
+else
+answer="y"
+fi
+if [ "$answer" == "y" ]; then
+echo "Overwriting the .bash_profile file..."
+cp -f .bash_profile $HOME/.bash_profile
+echo "Done setting up the bash, you need to restart the terminal for settings to take effect!"
+else
+echo "You have canceled the .bash_profile override..."
+fi
+}
+
+detectXcodeTools
+detectBrew
+upgradeBash
+installSoft
+updateProfile
+echo "All done"
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# MacOS_Shell
+# What is this ?
+
+This is actually a collection of scripts/configuration values for your MacOS Terminal visual and general improvements, it comes with easy to use installer, it also installs Brew.sh Unix Ports Collection package manager too!
+It updates the the original bash from macOS to version 5, also does some changes to visual improve your terminal experience, by enabling colors and other usefull visual effects!
+All you need is to download these files and run ```./Install.sh```
+
+# How it looks ?
+
+![test](/pics/bash_some_features.png)
+
+
+# MacOS Shell Installation
+
+```
+./Install.sh
+```
+And follow the instructions
+
+# TODO
+
+I've got some ideas in making it actually self-updating collection of bash scripts for most common tasks in mind! Feel free to ask for any request if you need under project Issues TAB, thanks!
